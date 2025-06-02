7ad6fa29ed87b493d3fa2ece719f52a137b6353e
Nick Phillips <51008939+8h9x@users.noreply.github.com>
2025-06-02 03:38:39 -0400

    init new bot base

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-    println("Hello, Blast!")
+	_, isProd := os.LookupEnv("PROD")
+	if !isProd {
+		err := godotenv.Load(".env")
+		if err != nil {
+			slog.Error(fmt.Sprintf("Error loading .env file: %s", err))
+			os.Exit(1)
+		}
+	}
+
+	client, err := disgo.New(os.Getenv("DISCORD_BOT_TOKEN"),
+		bot.WithGatewayConfigOpts(
+			gateway.WithIntents(
+				gateway.IntentGuilds,
+				// 				gateway.IntentGuildMessages,
+				gateway.IntentDirectMessages,
+			),
+		),
+		bot.WithEventListenerFunc(func(e *events.Ready) {
+			slog.Info("Connected to discord gateway")
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = client.OpenGateway(context.TODO()); err != nil {
+		panic(err)
+	}
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+	<-s
 }
