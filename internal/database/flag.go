6035b70b22b00c6d343338a05a44ad734b40dd11
Nick Phillips <itsdistrust@icloud.com>
2025-12-27 15:50:20 -0500

    Create flag.go

---
+++ internal/database/flag.go
@@ -0,0 +1,17 @@
+package database
+
+type Flag uint32
+
+func (f *Flag) Has(flag Flag) bool { return *f&flag != 0 }
+func (f *Flag) Set(flag Flag)     { *f |= flag }
+func (f *Flag) Toggle(flag Flag)  { *f ^= flag }
+func (f *Flag) Clear(flag Flag)   { *f &= ^flag }
+
+const (
+    FlagUser Flag = 0
+
+	FlagDeveloper Flag = 1 << iota
+    FlagBetaTester
+    FlagVIP
+    FlagPremiumUser
+)
